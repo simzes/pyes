9901a74cdce796eb2705c5fe3ff046831424ffd5
simon <dev@simonsbench.net>
2020-05-26 14:06:52 -0400

    Adding skeleton project code
    Loads catalog from preinstalled directory, or downloaded/updated
    catalog. Prefers updated catalog.
    
    UI has catalog elements placed through, with no styling. Loading is
    not implemented.

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,8 @@
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, ipcMain } from 'electron'
+
+const axios = require('axios')
+const jetpack = require('fs-jetpack')
+const path = require('path')
 
 /**
  * Set `__static` path to static files in production
@@ -20,7 +24,11 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     height: 563,
     useContentSize: true,
-    width: 1000
+    width: 1000,
+    webPreferences: {
+      nodeIntegration: true,
+      webSecurity: false
+    }
   })
 
   mainWindow.loadURL(winURL)
@@ -63,3 +71,213 @@ app.on('ready', () => {
   if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
 })
  */
+
+class Catalog {
+  REQUIRED_FILES = [
+    'image',
+//    'description',
+    'binary',
+  ]
+
+  constructor(source_path, is_preinstalled, remote_source) {
+    this.source_path = source_path
+    this.is_preinstalled = is_preinstalled
+    this.remote_source = remote_source
+
+    this.contents = jetpack.read(path.join(this.source_path, 'index.json'), 'json');
+  }
+
+  load_remote_contents() {
+    let agg = []
+
+    if (this.remote_source) {
+      for (const {entry, relative_path} of this._files()) {
+        agg.push(download_catalog_path(this.remote_source, this.source_path, relative_path));
+      }
+    }
+
+    return Promise.all(agg);
+  }
+
+  get catalog() {
+    return this.contents
+  }
+
+  get entries() {
+    return this.contents.catalog
+  }
+
+  * _files(entry_files=null) {
+    /*
+      returns an iterator over all the files in the catalog, with the form {entry: entry, relative_path: <path>}
+    */
+    if (entry_files == null) {
+      entry_files = this.REQUIRED_FILES
+    }
+
+    for (const entry of this.entries) {
+      for (const file of entry_files) {
+        yield {
+          entry: entry,
+          relative_path: path.join(entry.label, entry[file])
+        }
+      }
+    }
+  }
+
+  validate_catalog() {
+    for (var {entry, relative_path} of this._files()) {
+      const file_path = path.join(this.source_path, relative_path)
+      if (!jetpack.exists(file_path)) {
+        console.log(`Missing catalog file: ${file_path}`);
+      }
+    }
+  }
+
+  localize_catalog() {
+    const prefix = this.is_preinstalled ? '' : 'file://'
+
+    for (const entry of this.entries) {
+      for (const file_field of this.REQUIRED_FILES) {
+        if (!entry[file_field]) {
+          continue;
+        }
+        entry[file_field] = prefix + path.join(this.source_path, entry.label, entry[file_field])
+      }
+    }
+  }
+
+  move_catalog(dest_path) {
+//    jetpack.remove(dest_path);
+//    jetpack.move(this.source_path, dest_path);
+
+    delete this.contents;
+  }
+
+  static get preinstalled_path() {
+    return 'static/catalog';
+  }
+
+  static get updated_path() {
+    return path.join(app.getPath('userData'), 'catalog');
+  }
+
+  static get downloaded_path() {
+    return path.join(app.getPath('userData'), 'tmp_catalog');
+  }
+}
+
+function load_catalog(source, is_preinstalled=false, remote_source=null) {
+  console.log('loading catalog from path: ' + source)
+
+  const catalog = new Catalog(source, is_preinstalled, remote_source);
+
+  return catalog.load_remote_contents()
+    .then(() => {
+      console.log("validating/localizing content")
+
+      catalog.validate_catalog();
+      catalog.localize_catalog();
+
+      console.log('Catalog loaded and validated successfully');
+      console.log('Catalog path: ' + catalog.source_path)
+      console.log('Catalog contents: ' + JSON.stringify(catalog.catalog))
+
+      return catalog;
+    }).catch((error) => {
+      console.log('error in catalog creation:' + error)
+      console.log(error.stack)
+
+      throw error;
+    })
+}
+
+async function find_catalog() {
+  /*
+    Find the appropriate catalog; prefers the recently downloaded catalog to
+    any previously downloaded catalog. Prefers a previously downloaded one to
+    the preinstalled catalog.
+
+    As side effects, this kicks off a new download attempt, and also consolidates
+    any newly downloaded catalog into the current updated catalog.
+  */
+  return load_catalog(Catalog.downloaded_path)
+    .then(async (catalog) => {
+      console.log("have newly downloaded catalog -- moving into updated catalog")
+      catalog.move_catalog(catalog.updated_path);
+      return load_catalog(catalog.updated_path);
+    })
+    .catch(error => load_catalog(Catalog.updated_path))
+    .catch(error => load_catalog(Catalog.preinstalled_path))
+    .finally((catalog) => {
+      // kick off download attempt of catalog
+      load_remote_catalog();
+      return catalog;
+    })
+}
+
+function load_remote_catalog() {
+  /*
+    Check the catalog for updates, downloading it into the downloaded path if it has changed
+  */
+  const catalog_url = "https://raw.githubusercontent.com/simzes/test-bitty-catalog/test-progcat/";
+  const index_path = 'index.json';
+
+  const dest_base = Catalog.downloaded_path;
+
+  return download_catalog_path(catalog_url, dest_base, index_path)
+    .then(() => {
+      console.log('downloaded catalog index successfully');
+
+      const catalog = load_catalog(dest_base, false, catalog_url);
+
+      console.log("catalog contents: " + JSON.stringify(catalog.catalog))
+
+      return catalog;
+    }).catch((error) => {
+      console.log('error with catalog update ' + index_path + ': ' + error);
+      console.log(error.stack);
+
+      jetpack.remove(dest_base);
+
+      throw error;
+    });
+}
+
+function download_catalog_path(base_url, base_path, download_path) {
+  const source_url = base_url + download_path;
+  const dest_path = path.join(base_path, download_path);
+
+  console.log("requesting: " + source_url + "\n    into: " + dest_path)
+
+  return axios.get(source_url, {responseType: 'arraybuffer'})
+   .then(response => {
+      console.log("download for " + dest_path + " successful: " + response.status);
+      jetpack.write(dest_path, response.data);
+      return dest_path;
+    }).catch(error => {
+      console.log("download for " + dest_path + " unsuccessful");
+      throw error;
+    })
+}
+
+const download_url = "https://upload.wikimedia.org/wikipedia/commons/5/5b/Name.jpg"
+
+const data_path = app.getPath('userData');
+  axios.get(download_url, {responseType: 'arraybuffer'})
+  .then(response => {
+    console.log("writing picture to: " + data_path);
+    jetpack.write(path.join(data_path, 'test_download.jpg'), response.data);
+  });
+
+ipcMain.on('catalog', async (event) => {
+  console.log('loading catalog for window');
+  const catalog = await find_catalog()
+
+  event.returnValue = catalog.entries
+});
+
+ipcMain.on('load_program', (event, program_label) => {
+  console.log('loading program with label: ' + program_label);
+  event.returnValue = "load program result";
+});
--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,20 +1,97 @@
 <template>
   <div id="app">
-    <landing-page></landing-page>
+    <h3> {{ selection ? selection.title : "" }} </h3>
+    <div class="catalogPane">
+      <template v-for="entry in catalog">
+        <div class="entryTile">
+          <p> {{ entry.title }} </p>
+          <p> label: {{ entry.label }} </p>
+          <img v-bind:src="entry.image"
+               v-on:click="entrySelect(entry.label)"
+          />
+          <p> description: {{ entry.description }} </p>
+        </div>
+      </template>
+    </div>
+    <div class="selectionPane">
+      <button v-on:click="upload">
+        Upload
+      </button>
+      <div class="selectionTile">
+        <h3> {{ selection.title }} </h3>
+        <p class="selectionDescription">
+          {{ selection.description }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import LandingPage from '@/components/LandingPage'
+  const { remote } = window.require('electron')
+  const {  ipcRenderer } = require('electron');
 
   export default {
-    name: 'progcat_arduino',
-    components: {
-      LandingPage
+    name: 'ToDos',
+    data: () => {
+      return {
+        stuff: "stuff2",
+        selection: { title: "stuff", description: "more stuff" },
+        catalog: [
+          {
+            label: "elephant",
+            title: "Elephant Bitty",
+            image: "static/elephant.jpg",
+            description: "what is an elephant even?",
+          },
+          {
+            label: "donkey",
+            title: "Donkey Bitty",
+            image: "static/donkey.jpg",
+            description: "what is a donkey even?",
+          }
+        ]
+      }
+    },
+    methods: {
+      entrySelect: function (label) {
+        console.log("selected: " + label);
+        this.selection = this.entryFromLabel(label);
+      },
+      upload: function () {
+        console.log("upload!");
+        if (this.selection.label) {
+          this.uploadAsync();
+        }
+      },
+      uploadAsync: async function () {
+          this.interval = setInterval(function () {
+            this.bumpLoading()
+          }.bind(this), 750);
+
+        return new Promise((resolve, reject) => {
+          console.log('load program result: ' + this.$electron.ipcRenderer.sendSync('load_program', this.selection.label));
+          clearInterval(this.interval);
+        })
+      },
+      bumpLoading: function () {
+        console.log('loading bump!');
+      },
+      entryFromLabel: function (label) {
+        for (var entry of this.catalog)
+          if (label === entry.label)
+            return entry;
+        return null;
+      },
+      handle_catalog: function(catalog) {
+        console.log('handling catalog: ' + catalog);
+      }
+    },
+    mounted: function () {
+      this.catalog = this.$electron.ipcRenderer.sendSync('catalog');
     }
   }
 </script>
-
 <style>
   /* CSS */
 </style>
