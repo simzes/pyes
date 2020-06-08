3ad6c603ce0e2324175f8d46f092d08592b1f00d
simon <dev@simonsbench.net>
2020-06-07 23:59:03 -0400

    Changing catalog index to more information at the top

--- src/main/index.js
+++ src/main/index.js
@@ -104,8 +104,8 @@ class Catalog {
     let agg = []
 
     if (this.remote_source) {
-      if (this.contents.landing) {
-        agg.push(download_catalog_path(this.remote_source, this.source_path, this.contents.landing.description))
+      if (this.contents.source.description) {
+        agg.push(download_catalog_path(this.remote_source, this.source_path, this.contents.source.description))
       }
       for (const {entry, property} of this._properties()) {
         agg.push(download_catalog_path(this.remote_source, this.source_path, path.join(entry.path, entry[property])));
@@ -175,11 +175,13 @@ class Catalog {
   convert_markdown() {
     const converter = new showdown.Converter();
 
-    if (this.contents.landing) {
-      const markdown_contents = jetpack.read(path.join(this.source_path, this.contents.landing.description))
+    this.catalog.landing = { title: "", markdown: "" }
+
+    if (this.contents.source.description) {
+      const markdown_contents = jetpack.read(path.join(this.source_path, this.contents.source.description))
+
+      this.contents.landing.title = this.contents.source.name
       this.contents.landing.markdown = converter.makeHtml(markdown_contents)
-    } else {
-      this.catalog.landing = { title: "", markdown: "" }
     }
 
     for (var {entry, property} of this._properties(["description"])) {
